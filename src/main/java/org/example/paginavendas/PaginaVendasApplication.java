7428faaac9c176b1c9dcc008af9bb4613553913c
GustavoHeimburg <gutogustavogu@gmail.com>
2024-07-23 15:54:02 -0300

    up

---
+++ src/main/java/org/example/paginavendas/PaginaVendasApplication.java
@@ -0,0 +1,13 @@
+package org.example.paginavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaginaVendasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/paginavendas/PaginaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.paginavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaVendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
